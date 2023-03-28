6022eb1edaf0d044a7ef23c9e04844e8628650ec
faridul22 <96929890+faridul22@users.noreply.github.com>
2023-03-28 21:50:17 +0600

    set card to localStorage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Card from '../Card/Card';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -13,10 +14,17 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, [])
 
+    useEffect(() => {
+        const storedCard = getShoppingCart();
+
+        console.log(storedCard)
+    }, [])
+
     const handleAddToCard = (product) => {
         // card.push(product) ==> Normal js hilay aivabay kaj hoito
         const newCard = [...card, product];
         setCard(newCard)
+        addToDb(product.id)
     }
 
     return (
